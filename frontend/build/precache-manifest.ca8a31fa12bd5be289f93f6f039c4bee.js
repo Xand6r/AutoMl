b24f8f368f135e8b78cb57d972f31facf4b59b2f
Alexander <ashuaibu@getcarbon.co>
2020-08-17 16:12:58 +0100

    [add] dist

---
+++ frontend/build/precache-manifest.ca8a31fa12bd5be289f93f6f039c4bee.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563567dfc508557b7f257e7b5a84cf54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7490a00cb53433bcdfe9",
+    "url": "/static/css/2.5afbf740.chunk.css"
+  },
+  {
+    "revision": "7253a8a261398096dcd4",
+    "url": "/static/css/main.969d281d.chunk.css"
+  },
+  {
+    "revision": "7490a00cb53433bcdfe9",
+    "url": "/static/js/2.1d69c059.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1d69c059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7253a8a261398096dcd4",
+    "url": "/static/js/main.24cf085d.chunk.js"
+  },
+  {
+    "revision": "0e690c04bc6d958d729a",
+    "url": "/static/js/runtime-main.d5e54e2d.js"
+  },
+  {
+    "revision": "12d6724a254d3be629fc6b2871ae5a6a",
+    "url": "/static/media/FontFont_FF.Mark.Pro.12d6724a.otf"
+  },
+  {
+    "revision": "476d44b0f6c8939bb8859c9ce7598310",
+    "url": "/static/media/FontFont_FF.Mark.Pro.Bold.476d44b0.otf"
+  },
+  {
+    "revision": "8531ae94f5ad973be8b718f88e9660ed",
+    "url": "/static/media/FontFont_FF.Mark.Pro.Medium.8531ae94.otf"
+  },
+  {
+    "revision": "dc5d54199e155be07213f553f89e27ea",
+    "url": "/static/media/calendar.dc5d5419.svg"
+  },
+  {
+    "revision": "3263f579551f92f168de7ac40b37168f",
+    "url": "/static/media/cliseTag.3263f579.svg"
+  },
+  {
+    "revision": "51629ca49533f87c75098951e8a4a787",
+    "url": "/static/media/notification.51629ca4.svg"
+  },
+  {
+    "revision": "038ce55171d608d8f534b4220d62b55f",
+    "url": "/static/media/playIcon.038ce551.svg"
+  },
+  {
+    "revision": "3af9fdcb965bf2ae8ca7c5d542701a4b",
+    "url": "/static/media/profileIcon.3af9fdcb.svg"
+  },
+  {
+    "revision": "5b586866dbbe1dc80940a2b928f7f2bc",
+    "url": "/static/media/samplepicture.5b586866.svg"
+  }
+]);
